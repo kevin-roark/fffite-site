7ce4310cf5ae8bfd5088ad1676103e6dc42f6cff
Kevin Roark <ker2143@columbia.edu>
2016-02-24 21:36:04 -0500

    initial quantum and drive continuous dumps

---
+++ score.js
@@ -0,0 +1,30 @@
+
+var renderer = new frampton.Renderer({
+  mediaConfig: mediaConfig,
+  timeToLoadVideo: 5000
+});
+
+var firstSegment = newSequencedSegment();
+renderer.scheduleSegmentRender(firstSegment, 2000);
+
+function newSequencedSegment() {
+  var segments = [];
+
+  var videos = frampton.util.shuffle(mediaConfig.videos);
+  videos.forEach((video) => {
+    var segment = new frampton.VideoSegment(video);
+    segments.push(segment);
+  });
+
+  var sequencedSegment = new frampton.SequencedSegment({
+    segments: segments,
+    onStart: () => {
+      // once it starts, schedule the next loop with a new shuffle
+      var newSegment = newSequencedSegment();
+      var offset = sequencedSegment.msDuration();
+      renderer.scheduleSegmentRender(newSegment, offset);
+    }
+  });
+
+  return sequencedSegment;
+}
