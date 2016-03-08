1b2dbcd42730baaec7a83fea7d5e062397fdec64
Kevin Roark <ker2143@columbia.edu>
2016-03-07 22:39:30 -0500

    bourne 1_2 length pyramid

---
+++ bourne_1_2/length-pyramid.js
@@ -0,0 +1,39 @@
+
+var renderer = new frampton.Renderer({
+  mediaConfig: mediaConfig
+});
+
+var shortestToLongest = frampton.mediaArranger.durationSortedMedia(mediaConfig.videos);
+var longestToShortest = frampton.mediaArranger.durationSortedMedia(mediaConfig.videos, true);
+
+var firstSegment = newSequencedSegment();
+renderer.scheduleSegmentRender(firstSegment, 3000);
+
+function newSequencedSegment() {
+  var segments = [];
+
+  shortestToLongest.forEach((video) => {
+    var segment = new frampton.VideoSegment(video);
+    segments.push(segment);
+  });
+
+  longestToShortest.forEach((video, idx) => {
+    // don't include the longest video twice...
+    if (idx > 0) {
+      var segment = new frampton.VideoSegment(video);
+      segments.push(segment);
+    }
+  });
+
+  var sequencedSegment = new frampton.SequencedSegment({
+    segments: segments,
+    onStart: () => {
+      // once it starts, schedule the next loop with a new shuffle
+      var newSegment = newSequencedSegment();
+      var offset = sequencedSegment.msDuration();
+      renderer.scheduleSegmentRender(newSegment, offset);
+    }
+  });
+
+  return sequencedSegment;
+}
