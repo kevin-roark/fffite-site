aa95da9b787279197780df1cba6498bbf7ebb450
Kevin Roark <ker2143@columbia.edu>
2016-03-09 18:47:44 -0500

    generate films for bourne_1 2 and 3

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ configs=(
   "bourne_1_1" "bourne_1_2" "bourne_1_3" "bourne_1_4" "bourne_1_5"
   "bourne_2_1" "bourne_2_2" "bourne_2_4"
   "bourne_3_1" "bourne_3_3" "bourne_3_4" "bourne_3_4_run" "bourne_3_4_fight" "bourne_3_6"
-  "bourne" "bourne_fight" "bourne_run" "bourne_chase"
+  "bourne_1" "bourne_2" "bourne_3"
+  "bourne_fight" "bourne_run" "bourne_chase"
+  "bourne"
 )
 
 # clear existing build
@@ -17,6 +19,7 @@ do
   for score in "${scores[@]}"
   do
     combo=$config"__"$score
+    echo "handling" $combo
 
     # make new folder and copy template
     mkdir films/$combo
@@ -28,7 +31,7 @@ do
     mv js/build.js films/$combo/js
 
     # add link to film-links.txt
-    echo '<a href="/films/'$combo'/" id="'$combo'-link" class="'$config'-link '$score'-link"></a>' >> template/film-links.txt
+    echo '<a href="/films/'$combo'/" id="'$combo'-link" class="token-link '$config'-link '$score'-link">fffight.site '$combo' film</a>' >> template/film-links.txt
 
   done
 done
