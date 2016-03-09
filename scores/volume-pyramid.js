c2adfd9a4976c70443234622b2e0e79aabb1c8f6
Kevin Roark <ker2143@columbia.edu>
2016-03-08 19:44:08 -0500

    start creating scores and media_config directories

--- scores/volume-pyramid.js
+++ scores/volume-pyramid.js
@@ -1,19 +1,29 @@
 
 var renderer = new frampton.Renderer({
   mediaConfig: mediaConfig,
-  timeToLoadVideo: 5000
+  audioFadeDuration: 80
 });
 
+var quietestToLoudest = frampton.mediaArranger.volumeSortedMedia(mediaConfig.videos);
+var loudestToQuietest = frampton.mediaArranger.volumeSortedMedia(mediaConfig.videos, {descending: true});
+
 var firstSegment = newSequencedSegment();
-renderer.scheduleSegmentRender(firstSegment, 2000);
+renderer.scheduleSegmentRender(firstSegment, 3000);
 
 function newSequencedSegment() {
   var segments = [];
 
-  var videos = frampton.util.shuffle(mediaConfig.videos);
-  videos.forEach((video) => {
+  quietestToLoudest.forEach((video) => {
     var segment = new frampton.VideoSegment(video);
     segments.push(segment);
+  });
+
+  loudestToQuietest.forEach((video, idx) => {
+    // don't include the loudest video twice...
+    if (idx > 0) {
+      var segment = new frampton.VideoSegment(video);
+      segments.push(segment);
+    }
   });
 
   var sequencedSegment = new frampton.SequencedSegment({
