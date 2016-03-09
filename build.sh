8a6ab30d5c26653a18a26a9d160233ffd834e6a5
Kevin Roark <ker2143@columbia.edu>
2016-03-09 01:33:15 -0500

    generate all kinds of media config

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 
 scores=( "continuous" "continuous-fades" "continuous-short-clips" "length-pyramid" "volume-pyramid" )
-configs=( "bourne_1_2" "bourne_3_4_chase" "bourne_3_4_fight" "bourne_3_4" )
+configs=(
+  "bourne_1_1" "bourne_1_2" "bourne_1_3" "bourne_1_4" "bourne_1_5"
+  "bourne_2_1" "bourne_2_2" "bourne_2_4"
+  "bourne_3_1" "bourne_3_3" "bourne_3_4" "bourne_3_4_run" "bourne_3_4_fight" "bourne_3_6"
+  "bourne" "bourne_fight" "bourne_run" "bourne_chase"
+)
 
 for config in "${configs[@]}"
 do
